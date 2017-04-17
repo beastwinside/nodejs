ad02c0e41d2ce9341e35cf5a8ad59fc7fb625294
wjm <1584824430@qq.com>
2017-04-17 21:42:47 +0800

    跨域问题解决

---
+++ mysql/ajax test/test.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+ 
+
+app.get('/testt.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "testt.html" );
+})
+ 
+app.get('/', function (req, res) {
+ 
+   
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   res.end("草草草草");
+})
+ 
+
+var server = app.listen(8081);
+
+ // http.createServer(function(req, res){
+ //  res.writeHead(200, {'Content-type' : 'text/plain;'});
+ //  res.write("hello world");
+ //  console.log('23');
+ //  res.end();
+ // }).listen(3000);
